046005ab71c338ffe15d5f0df86fa58f2beddc15
Kyle Cleary <kscleary11@gmail.com>
2024-07-01 15:12:35 -0600

    Declared, initialized, interpolated.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Dennis";
+            int homeRuns = 38;
+            char hittingGrade = 'A';
+            bool willBe = (homeRuns >= 35);
+            string lower = willBe.ToString().ToLower();
+            
+           
+
+            double numberOfPlayersWithGradeA = 10;
+            decimal percentageOfPlayersWithGradeA = 0.513M;
+
+
+            Console.WriteLine($"It is {lower}, {name} will be considered for induction into the Baseball Hall of Fame, since he ended the season with {homeRuns} home runs. This earned him a hitting grade of \"{hittingGrade}\". The number of players with this grade was {numberOfPlayersWithGradeA}. This was {percentageOfPlayersWithGradeA} percent of all players in the league.");
         }
     }
 }
